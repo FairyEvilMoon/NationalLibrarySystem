19fadbe222fce693ab47f6b631151f5ea39ab60d
FairyEvilMoon <94723756+FairyEvilMoon@users.noreply.github.com>
2023-05-02 08:54:18 +0400

    Update google_books_api.dart

--- national_library_system/lib/services/google_books_api.dart
+++ national_library_system/lib/services/google_books_api.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class GoogleBooksApi {
-  static const String _apiKey = 'AIzaSyD6EUeGo1H0t4p9ZI585wGozAxAZ4jTxQ0';
+  static const String _apiKey = 'YOUR API KEY HERE';
   static const String _baseUrl = 'https://www.googleapis.com/books/v1/volumes';
 
   Future<List<dynamic>> searchBooks(String query) async {
